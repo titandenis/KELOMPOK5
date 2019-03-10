34ff22b2d64241f38343933093ca41f889a68561
uniqfadilah <uniqfadilah@gmail.com>
2019-03-10 20:52:45 +0700

    view biodata

---
+++ view/dashboard.php
@@ -0,0 +1,9 @@
+
+            <div class=container; style="padding-top: 40px;">
+            <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Selamat Datang, Mahasiswa!</h4>
+            <p>Silahkan melakukan registrasi dengan pembayaran kebank yang telah ditentukan oleh fakultas terlebih dahulu</p>
+             <hr>
+             <p class="mb-0">No.rek BRI 0105.01.001006.30.4.</p>
+			</div>
+			</div>
